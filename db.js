259451e6abbc522b0d4bc2d80e554c9dcafcb5ad
EDA <eda@mac-mini-1.local>
2017-05-11 16:40:25 +1200

    posting and updating two tables now working

--- db.js
+++ db.js
@@ -2,7 +2,9 @@
 module.exports = {
   getUser: getUser,
   getUsers: getUsers,
-  getProfile: getProfile
+  getProfile: getProfile,
+  addUser: addUser,
+  addProfile: addProfile
 }
 
 function getUsers (connection) {
@@ -18,3 +20,28 @@ function getProfile(id, connection){
     .join('profiles','users.id','profiles.user_id')
     .where('users.id', id)
 }
+
+function addUser (newUserObj, connection){
+  let data = {
+    name: newUserObj.name,
+    email: newUserObj.email
+  }
+  return connection('users')
+    .insert(data)
+}
+
+function addProfile (profileData, userId, connection){
+ // add the age from here
+ let data = {
+   age: profileData.age,
+   user_id: userId
+ }
+ return connection('profiles')
+   .insert(data)
+   //.exec()
+}
+
+// Add user
+// add name and email to users table
+// get the userid of that new entry
+// with that userid we insert an age value into profiles table
--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,29 @@ router.get('/profile/:userid', function (req, res) {
     })
 })
 
+
+router.get('/add', function (req, res) {
+  res.render('addUser')
+})
+
+
+router.post('/add', function (req, res){
+
+    let newUserObj = {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age
+    }
+
+console.log('new user object is: ' + newUserObj)
+
+    db.addUser(newUserObj,req.app.get('connection'))
+      .then(function (result) {
+        db.addProfile(newUserObj, result[0], req.app.get('connection'))
+      .then(function(){
+          res.redirect('/')
+        })
+      })
+})
+
 module.exports = router
